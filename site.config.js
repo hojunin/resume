dc9fb28174cdc91e2f8a92cd3b7e5d0ab74ac0be
hojunin <dlsghwns@naver.com>
2021-10-25 23:03:58 +0900

    왜 안될까

--- site.config.js
+++ site.config.js
@@ -1,51 +1,20 @@
 module.exports = {
-  // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'hjinn/9b036ea279d04498ba907620e641c475',
-
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionPageId: '4c2e45ca9a984157b5043077a95a921b',
   rootNotionSpaceId: null,
-
-  // basic site info (required)
   name: 'hjinn',
   domain: 'hjinn.it',
   author: 'Hojun In',
-
-  // open graph metadata (optional)
   description: 'Example site description',
   socialImageTitle: 'Transitive Bullshit',
   socialImageSubtitle: 'Hello World! 👋',
-
-  // social usernames (optional)
   twitter: 'hjinn',
   github: 'hojunin',
   linkedin: 'hjinn',
-
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
-
-  // image CDN host to proxy all image requests through (optional)
-  // NOTE: this requires you to set up an external image proxy
   imageCDNHost: null,
-
-  // Utteranc.es comments via GitHub issue comments (optional)
   utterancesGitHubRepo: null,
-
-  // whether or not to enable support for LQIP preview images (optional)
-  // NOTE: this requires you to set up Google Firebase and add the environment
-  // variables specified in .env.example
   isPreviewImageSupportEnabled: false,
-
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
   pageUrlOverrides: null
 }
